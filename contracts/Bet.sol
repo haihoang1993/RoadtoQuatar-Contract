d925d26b4c135928c359d11ebf33b86cebfaee9d
macbook <trunghanguyen98@gmail.com>
2022-08-03 16:22:13 +0700

    update

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -29,9 +29,14 @@ contract Bet is Ownable {
 
      BetDetail[] listBet;
      mapping (address => EnumerableSetUpgradeable.UintSet) betUser;
+
      uint256 public countBetTeamA;
      uint256 public countBetTeamDraw;
      uint256 public countBetTeamB;
+
+     uint256 amountTokenBetA;
+     uint256 amountTokenBetDraw;
+     uint256 amountTokenBetB;
 
      mapping(address => bool) usersBetA;
      mapping(address => bool) usersBetDraw;
@@ -56,23 +61,74 @@ contract Bet is Ownable {
         listBet.push(save);
 
        if(bet==1) {
+           amountTokenBetA+=amountTokenBet;
            if(!usersBetA[msg.sender]){
                usersBetA[msg.sender]=true;
                countBetTeamA++;
            }
              
        } else if(bet==3) {
+           amountTokenBetB+=amountTokenBet;
            if(!usersBetB[msg.sender]){
                usersBetB[msg.sender]=true;
                countBetTeamB++;
            }
        }  else {
+           amountTokenBetDraw+=amountTokenBet;
             if(!usersBetDraw[msg.sender]){
                usersBetDraw[msg.sender]=true;
                countBetTeamDraw++;
            }
        }
-        
+    
     }
 
+    function cliamBet() public {
+
+    }
+
+    function getTokenRewardBet() external view returns(uint256) {
+        BetObj memory _bet= factory.getBetInfo(address(this));
+        if(_bet.status!=Status.Finished) {
+            return 0;
+        }
+
+        uint resultBet = _bet.goalTeamA > _bet.goalTeamB ? 1 : (_bet.goalTeamA==_bet.goalTeamB?2 : 3);
+
+        if(resultBet==1){
+            return (amountTokenBetDraw + amountTokenBetB).div(countBetTeamA);
+        } 
+
+        if(resultBet==2){
+            return (amountTokenBetA + amountTokenBetB).div(countBetTeamDraw);
+        }
+
+         if(resultBet==3){
+            return (amountTokenBetA + amountTokenBetDraw).div(countBetTeamB);
+        }
+    }
+
+    function getTokenBeted(address _adr, uint typeBet) private view returns(uint256) {
+        uint256 total=0;
+        for(uint256 i=0; i<betUser[_adr].length();i++){
+            BetDetail memory _bet = listBet[betUser[_adr].at(i)];
+            if(_bet.voteBet==typeBet){
+                total=total.add(_bet.amoutTokenBet);
+            }
+        }
+        return total;
+    }
+
+    function getAllBet() external view returns(BetDetail[] memory){
+        return listBet;
+    }
+
+    function getBetUser(address _adr) external view returns (BetDetail[] memory){
+         BetDetail[] memory arr = new BetDetail[](3);
+         for(uint256 i=0; i<betUser[_adr].length();i++){
+            BetDetail memory _bet = listBet[betUser[_adr].at(i)];
+            arr[i]=_bet;
+        }
+        return arr;
+    }
 }
